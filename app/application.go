7d609ba6f142160aae98d0e6c2afe6a462dfa4ec
Joel Shin <joel@fluxxlabs.com>
2020-09-23 23:44:24 -0700

    cleanup

--- app/application.go
+++ app/application.go
@@ -8,12 +8,8 @@ var (
 	r = gin.Default()
 )
 
+// StartApplication establishes endpoints and starts server
 func StartApplication() {
-	// r.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"message": "pong",
-	// 	})
-	// })
 	mapUrls()
 	// logger.Info("about to start the application...")
 	r.Run(":8082") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/shin888shin/parks/app"
 )
 
 func main() {
-	fmt.Println("+++> parks")
-
 	app.StartApplication()
 }
