cbf0086bef70be1cfb69f98a040abee6faa848ea
Joel Shin <joel@fluxxlabs.com>
2020-09-26 09:29:24 -0700

    start datasource file

---
+++ datasources/mysql/parks/parks_db.go
@@ -0,0 +1,17 @@
+package parks
+
+import (
+	"database/sql"
+)
+
+var (
+	parksDB *sql.DB
+)
+
+func inti() {
+	// var err error
+	// parksDB, err = sql.Open("mysql", "")
+	// if err != nil {
+	// 	panic(err)
+	// }
+}
