64f862f88876bb6e43affc5d31868a317e7b7fa1
Joel Shin <joel@fluxxlabs.com>
2020-09-28 14:55:21 -0700

    handle possible error for unique on save

--- domain/parks/park_dao.go
+++ domain/parks/park_dao.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/go-sql-driver/mysql"
+
 	"github.com/shin888shin/parks/datasources/mysql/parks"
 	"github.com/shin888shin/parks/utils/date"
 	"github.com/shin888shin/parks/utils/errors"
@@ -23,8 +25,15 @@ func (park *Park) Get() *errors.RestErr {
 	}
 	defer stmt.Close()
 	getResult := stmt.QueryRow(park.ID)
+
 	if err := getResult.Scan(&park.ID, &park.Name, &park.Description, &park.Location, &park.CreatedAt); err != nil {
-		fmt.Println(err)
+		// fmt.Println("+++> here now")
+		// sqlErr, ok := err.(*mysql.MySQLError)
+		// if !ok {
+		// 	return errors.NewInternalServerErr(fmt.Sprintf("error getting park %d: %s", park.ID, err.Error()))
+		// }
+		// fmt.Println("+++> number:", sqlErr.Number)
+		// fmt.Println("+++> msg:", sqlErr.Message)
 		if strings.Contains(err.Error(), errorNoRows) {
 			return errors.NewNotFoundErr(fmt.Sprintf("park %d not found", park.ID))
 		}
@@ -45,10 +54,17 @@ func (park *Park) Save() *errors.RestErr {
 
 	insertResult, err := stmt.Exec(park.Name, park.Description, park.Location, park.CreatedAt)
 	if err != nil {
-		// TODO: maybe turn on name uniqueness
-		// if strings.Contains(err.Error(), "name_UNIQUE") {
-		// 	return errors.NewBadRequestErr(fmt.Sprintf("name %s must be unique ", park.Name))
-		// }
+		sqlErr, ok := err.(*mysql.MySQLError)
+		if !ok {
+			return errors.NewInternalServerErr(fmt.Sprintf("error when saving park: %s", err.Error()))
+		}
+		fmt.Println("+++> number:", sqlErr.Number)
+		fmt.Println("+++> msg:", sqlErr.Message)
+		switch sqlErr.Number {
+		case 1062:
+			// error: name_UNIQUE
+			return errors.NewInternalServerErr(fmt.Sprintf("park %s already exists", park.Name))
+		}
 		return errors.NewInternalServerErr(fmt.Sprintf("error on save: %s", err.Error()))
 	}
 
