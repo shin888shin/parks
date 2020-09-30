35d00dfdf8a0c7f9cbd2807577af23d669382d8f
Joel Shin <joel@fluxxlabs.com>
2020-09-30 16:37:34 -0700

    db config debug vs aws

--- datasources/mysql/parks/parks_db.go
+++ datasources/mysql/parks/parks_db.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	"io/ioutil"
 	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/olebedev/config"
 )
@@ -15,23 +18,30 @@ var (
 
 func init() {
 	var err error
+	var dsnStr string
+	var dsnErr error
 
-	file, err := ioutil.ReadFile("conf.json")
-	if err != nil {
-		panic(err)
-	}
-	jsonString := string(file)
+	if os.Getenv("DEBUG") == "1" {
+		log.Println("+++> running locally")
+		file, err := ioutil.ReadFile("conf.json")
+		if err != nil {
+			panic(err)
+		}
+		jsonString := string(file)
 
-	cfg, err = config.ParseJson(jsonString)
-	dsnStr, dsnErr := cfg.String("mysql.local.dsn")
-	if dsnErr != nil {
-		panic(err)
+		cfg, err = config.ParseJson(jsonString)
+		dsnStr, dsnErr = cfg.String("mysql.local.dsn")
+		if dsnErr != nil {
+			panic(err)
+		}
+	} else {
+		log.Println("+++> running aws")
+		// dsnStr = "root:root@tcp(mysql_parks:3306)/parks?parseTime=true"
+		dsnStr = "root:root@<aws-rds-endpoint>/parks?parseTime=true"
 	}
+
 	Client, err = sql.Open("mysql", dsnStr)
 	if err != nil {
-		panic(err)
-	}
-	if err = Client.Ping(); err != nil {
 		panic(err)
 	}
 
