496c9b43fd9db765624bc952186d9a43d424c3ca
Joel Shin <joel@fluxxlabs.com>
2020-10-06 13:58:13 -0700

    test work

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"net/http"
@@ -10,9 +11,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var testFlag = flag.String("DEBUG", "1", "xxdslfkj")
+// var testFlag = flag.String("DEBUG", "1", "xxdslfkj")
+var loc = flag.String("location", "World", "Name of location to greet")
 
 func TestPingRoute(t *testing.T) {
+	fmt.Printf("================================================\n")
+	fmt.Printf("===> 1: %+v\n", *loc)
+	fmt.Printf("================================================\n")
 	router := setupRouter()
 
 	w := httptest.NewRecorder()
@@ -25,22 +30,32 @@ func TestPingRoute(t *testing.T) {
 
 func TestParksRoute(t *testing.T) {
 	router := setupRouter()
+	var createJson = []byte(`{
+		"name": "aaa",
+		"description": "bbb",
+		"location": "ccc"}`)
+
+	var updateJson = []byte(`{
+			"name": "aaaaaa",
+			"description": "bbb",
+			"location": "ccc"}`)
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/parks", nil)
+
+	req, _ := http.NewRequest("POST", "/parks", bytes.NewBuffer(createJson))
 	router.ServeHTTP(w, req)
+	fmt.Printf("+++> CREATE: %+v\n", w.Body.String())
+	assert.Equal(t, 201, w.Code)
 
-	// fmt.Printf("+++> 0: %+v\n", *testFlag)
-	// fmt.Printf("+++> 1: %+v\n", w.Code)
-	fmt.Println("++++++++++++++++++++++++++++++++++++++++")
-	fmt.Println("++++++++++++++++++++++++++++++++++++++++")
-	fmt.Println("++++++++++++++++++++++++++++++++++++++++")
-	fmt.Printf("+++> 2: %+v\n", w.Body.String())
-	fmt.Println("++++++++++++++++++++++++++++++++++++++++")
-	fmt.Println("++++++++++++++++++++++++++++++++++++++++")
-	fmt.Println("++++++++++++++++++++++++++++++++++++++++")
+	req, _ = http.NewRequest("PUT", "/parks", bytes.NewBuffer(updateJson))
+	router.ServeHTTP(w, req)
+	fmt.Printf("+++> UPDATE: %+v\n", w.Body.String())
+	assert.Equal(t, 201, w.Code)
 
-	assert.Equal(t, 200, w.Code)
+	req, _ = http.NewRequest("GET", "/parks", nil)
+	router.ServeHTTP(w, req)
+	fmt.Printf("+++> LIST: %+v\n", w.Body.String())
+	assert.Equal(t, 201, w.Code)
 	// fails
 	// assert.Equal(t, `{"message":"pong"}`, w.Body.String())
 }
